8d6579696b091d90b67a2d945d54813f8adf7825
omranali2111 <aljabri1234@hotmail.com>
2023-06-15 10:54:04 +0400

    Problem 2: Count Vowels

--- c-sharp-string-manipulation/Program.cs
+++ c-sharp-string-manipulation/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System.ComponentModel;
+using static System.Console;
 internal class Program
 {
    
@@ -8,11 +9,14 @@ internal class Program
     {
         WriteLine("enter string");
         string string1= Convert.ToString(ReadLine());
-       
-
         WriteLine("the revers of the string is "+reversString(string1));
+        WriteLine("*************************************************");
+        WriteLine("enter string");
+        string string2 = Convert.ToString(ReadLine());
+        WriteLine("the number of Vowels in this string  is " + _countVowels(string2));
 
-       
+
+
         /*
         Problem 1: Reverse a String
         Write a function that takes a string as input and returns 
@@ -36,6 +40,29 @@ internal class Program
 
             return revstring;
 
+        }
+
+        /*
+        Problem 2: Count Vowels
+        Write a function that takes a string as input
+        and counts the number of vowels(a, e, i, o, u) 
+        present in the string.For example, if the input is
+        "Open", the function should return 2.
+        */
+
+        static int  _countVowels(string string2)
+        {
+            int count = 0;
+            for (int i =0; i < string2.Length; i++)
+            {
+
+                if (string2[i] =='a'|| string2[i] == 'e' || string2[i] == 'i' || string2[i] == 'o' || string2[i] == 'u')
+                {
+                     count ++;
+                }
+
+            }
+            return count;
         }
     }
 }

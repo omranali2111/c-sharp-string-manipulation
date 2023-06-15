df127a0c2c0a274051b38902e58b98296eadecea
omranali2111 <aljabri1234@hotmail.com>
2023-06-15 09:50:36 +0400

    Problem 1: Reverse a String

---
+++ c-sharp-string-manipulation/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+internal class Program
+{
+   
+    private static void Main(string[] args)
+
+
+    {
+        WriteLine("enter string");
+        string string1= Convert.ToString(ReadLine());
+       
+
+        WriteLine("the revers of the string is "+reversString(string1));
+
+       
+        /*
+        Problem 1: Reverse a String
+        Write a function that takes a string as input and returns 
+        the reverse of that string.For example, if the input
+        is "Hello", the function should return "olleH".Without 
+        using the built-in functions
+        */
+
+        static string reversString(string string1)
+
+        {
+            string revstring = string.Empty;//blank string
+            for (int i = string1.Length - 1; i >= 0; i--)
+            {
+
+                revstring += string1[i];
+
+
+            }
+
+
+            return revstring;
+
+        }
+    }
+}
